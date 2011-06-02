fe497b3f1b73fe54e6d38e33e9a46ccf513d6a49
jenjenbirdie <jennifer.birdie@gmail.com>
2011-06-01 22:55:58 -0700

    sample2-master

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,11 +1,7 @@
 class StoreController < ApplicationController
   def index
-	@products = Product.all
-<<<<<<< HEAD
-	cart = current_cart
-=======
-	@cart = current_cart
->>>>>>> dash-of-ajax
+    @products = Product.all
+    @cart = current_cart
   end
 
 end
